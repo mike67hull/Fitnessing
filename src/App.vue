92d64d6807188bd2e32e6a359662a97c66164630
Michael Hull <mike67hull@gmail.com>
2018-10-29 12:42:24 -0400

    Nav Init

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<div>
+    <div class="display-4">
+      <img alt="Vue logo" src="./assets/fit-logo.png" id="logo">
+      FITNESSING
+      </div>
+      <Nav />
+      <div id="app" class="container">
+      
     <router-view/>
   </div>
+  
+</div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+#logo { height: 50px;
+padding-left:10px;
+padding-bottom: 10px;
+padding-right: 0px;}
+.container{
+  width: 100%;
+  top: 0px;
 }
 </style>
+
+<script>
+// @ is an alias to /src
+import Nav from '@/components/Nav.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Nav
+  }
+}
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <nav vertical class="navbar-dark">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link class="nav-link" exact-active-class="active" to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" exact-active-class="active" to="/about">About</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" exact-active-class="active" to="/game">Game</router-link>
+      </li>
+    </ul>
+    </nav>
+    <!--
+  <nav tabs vertical class="w-10">
+    <nav-item active><router-link class="nav-link" exact-active-class="active" to="/">Home</router-link></nav-item>
+    <nav-item><router-link class="nav-link" exact-active-class="active" to="/about">About</router-link></nav-item>
+    <nav-item><router-link class="nav-link" exact-active-class="active" to="/game">Game</router-link></nav-item>
+  </nav>-->
+
+</template>
+
+<style lang="scss">
+.navbar-nav {
+    
+    height: 50%;
+    width: 240px;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    background-color: rgba(11, 31, 9, 0.6);
+    overflow-x: hidden;
+    padding-top: 20px;
+    padding-left: 20px;
+}
+
+</style>
